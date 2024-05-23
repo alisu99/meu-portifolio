04edb86bea08354e89f4dce3b913a1db31ed33e4
Alisson Alencar <alissonsts910@gmail.com>
2024-05-23 09:59:21 -0300

    ...

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,15 +1,48 @@
 const name = "Alisson!";
-const speed = 100; // Velocidade em milissegundos
-
+const speed = 100;
 let i = 0;
-function typeWriter() {
+
+function typeWriter(callback) {
     if (i < name.length) {
         document.getElementById("name").innerHTML += name.charAt(i);
         i++;
-        setTimeout(typeWriter, speed);
+        setTimeout(() => typeWriter(callback), speed);
+    } else {
+        if (callback) callback();
+    }
+}
+
+const cargo = "Desenvolvedor Full Stack"
+const velocidade = 50;
+let x = 0;
+
+function printCargo(callback) {
+    if (x < cargo.length) {
+        document.getElementById("cargo").innerHTML += cargo.charAt(x);
+        x++;
+        setTimeout(() => printCargo(callback), velocidade);
+    } else {
+        if (callback) callback();
+    }
+}
+
+const ola = "Olá, me chamo";
+let z = 0;
+
+function printOla(callback) {
+    if (z < ola.length) {
+        document.getElementById("ola").innerHTML += ola.charAt(z);
+        z++;
+        setTimeout(() => printOla(callback), 100);
+    } else {
+        if (callback) callback();
     }
 }
 
 window.onload = function () {
-    typeWriter();
+    printOla(() => {
+        typeWriter(() => {
+            printCargo();
+        });
+    });
 };
